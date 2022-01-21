185eca40b80906c35d30404248a4332fde6972ea
Alyaman Maasarani <alyaman.x.p300@gmail.com>
2022-01-21 19:54:28 +0200

    modified some files to transition to nixos

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
 set -a PATH /home/vex/.cargo/bin/
 set -a PATH /home/yaman/.local/share/bin/
-sh /home/yaman/.nix-profile/etc/profile.d/nix.sh
+set -a PATH /home/yaman/.emacs.d/bin/
+# sh /home/yaman/.nix-profile/etc/profile.d/nix.sh
