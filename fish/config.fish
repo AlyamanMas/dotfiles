58e561d87c6e84c302ca32e247172d0f0572631a
Alyaman Maasarani <alyaman.x.p300@gmail.com>
2022-01-30 22:45:05 +0200

    sourced Starship promopt in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,9 @@
+# `PATH` Extensions
 set -a PATH /home/vex/.cargo/bin/
 set -a PATH /home/yaman/.local/share/bin/
 set -a PATH /home/yaman/.emacs.d/bin/
+
+# Prompt (Starship)
+starship init fish | source
+
 # sh /home/yaman/.nix-profile/etc/profile.d/nix.sh
