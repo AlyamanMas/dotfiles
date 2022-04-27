6cedfdcd12e9e8237a76351664da14deb4bcb51c
Alyaman Maasarani <alyaman.x.p300@gmail.com>
2022-04-27 17:38:24 +0300

    added some fish aliases

---
+++ fish/functions/cl.fish
@@ -0,0 +1,3 @@
+function cl --wraps='cargo clippy' --description 'alias cl cargo clippy'
+  cargo clippy $argv; 
+end
---
+++ fish/functions/mp.fish
@@ -0,0 +1,3 @@
+function mp --wraps='mkdir -p' --description 'alias mp mkdir -p'
+  mkdir -p $argv; 
+end
